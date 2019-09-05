098ea656d15c0e0528dc8010fe51029dfe2813bf
EriChen <erichen0615@gmail.com>
2019-09-05 18:12:03 +0800

    merging

--- Train_Model/train.py
+++ Train_Model/train.py
@@ -32,7 +32,7 @@ def train_model(base_lr, loss, data_num):
     :return:
     train_op, lr_op
     """
-    lr_factor = 0.1
+    lr_factor = 0.5 # was 0.1
     global_step = tf.Variable(0, trainable=False)
     #LR_EPOCH [8,14]
     #boundaried [num_batch,num_batch]
