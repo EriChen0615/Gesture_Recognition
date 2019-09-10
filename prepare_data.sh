788b451614fdcfe7a969bcd1959634b88ce53498
EriChen <erichen0615@gmail.com>
2019-09-11 01:46:29 +0800

    prepare_data.sh bug fixed and running

--- prepare_data.sh
+++ prepare_data.sh
@@ -5,4 +5,4 @@ cd prepare_data
 python PNet_gen_data.py --im_dir ../Dataset/Training --anno_file imglist_with_gesture.txt --save_dir ../Training_Data/PNet-12
 python gen_gesture.py --net PNet --im_dir ../Dataset/Training --anno_file imglist_with_gesture.txt --save_dir ../Training_Data/PNet-12
 python merge_gesture_and_data.py --net PNet --base_dir ../Training_Data/PNet-12
-python gen_tfrecord --net PNet --data_dir ../Training_Data/PNet-12
+python gen_tfrecord.py --net PNet --data_dir ../Training_Data/PNet-12
