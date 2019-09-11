ca9545c56c0a371ff4c44fc85780966a30d04eed
EriChen <erichen0615@gmail.com>
2019-09-11 17:19:06 +0800

    working on PRO_train.sh

--- PRO_train.sh
+++ PRO_train.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-env_name='tf-gpu'
-output_dir='Training_Data'
-net_prefix='Model/MTCNN-test/PNet'
+env_name=tf-gpu
+model_name=MTCNN-test
+output_dir=Training_Data/$model_name
+net_prefix=Model/MTCNN-test
 
-raw_img_dir='Dataset/Training'
-anno_name='imglist_with_gesture.txt'
-
-pnet_prefix='Model/MTCNN-test/PNet'
-
-
-rnet_prefix='Model/MTCNN-test/RNet'
+raw_img_dir=Dataset/Training
+anno_name=imglist_with_gesture.txt
 
 
-model_name='MTCNN-test'
-tfrecord_dir='Training_Data/PNet-12/imglists'
+tfrecord_dir=imglists
 base_lr=0.1
 pend_epoch=5
 rend_epoch=5
@@ -22,34 +17,63 @@ oend_epoch=5
 
 
 # PNet data generation
+net=PNet
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate $env_name
 cd prepare_data
-python PNet_gen_data.py --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$poutput_dir
-python gen_gesture.py --net PNet --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$poutput_dir
-python merge_gesture_and_data.py --net PNet --base_dir ../$poutput_dir
-python gen_tfrecord.py --net PNet --data_dir ../$poutput_dir
-
+python PNet_gen_data.py --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir/$net
+python gen_gesture.py --net PNet --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir/$net
+python merge_gesture_and_data.py --net $net --base_dir ../$output_dir/$net
+python gen_tfrecord.py --net PNet --data_dir ../$output_dir/$net
+cd ..
 echo('PNet data generation completes!')
 
 # PNet training
 
-cd ..
 cd Train_Model
-python train_net.py --net $net --model_name $model_name --tfrecord_dir ../$tfrecord_dir/PNet --base_lr $base_lr --end_epoch $end_epoch
+python train_net.py --net $net --model_name $model_name --tfrecord_dir ../$output_dir/$net/$tfrecord_dir --base_lr $base_lr --end_epoch $end_epoch
 cd ..
+
+echo('PNet training completes!')
 
 # RNet data generation
-python gen_data.py --test_mode PNet --anno_file $anno_name --im_dir ../$raw_img_dir --save_dir ../$output_dir --epoch $pend_epoch --prefix ../$model_prefix
-python gen_gesture.py --net RNet --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir
-python merge_gesture_and_data.py --net RNet --base_dir ../$output_dir
-python gen_tfrecord.py --net RNet --data_dir ../$output_dir
+net=RNet
+cd prepare_data
+python gen_data.py --test_mode PNet --anno_file $anno_name --im_dir ../$raw_img_dir --save_dir ../$output_dir/$net --epoch $pend_epoch --prefix ../$net_prefix/PNet
+python gen_gesture.py --net RNet --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir/$net
+python merge_gesture_and_data.py --net $net --base_dir ../$output_dir/$net
+python gen_tfrecord.py --net RNet --data_dir ../$output_dir/$net
+cd ..
+
+echo('RNet data generation completes!')
+
+# RNet training
+cd Train_Model
+python train_net --net $net --model_name $model_name --tfrecord_dir ../$output_dir/$net/$tfrecord_dir --base_lr $base_lr --end_epoch $rend_epoch
+cd ..
+
+echo('RNet training completes')
 
 # ONet data generation
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate tf-gpu
+net=ONet
 cd prepare_data
-python gen_data.py --test_mode RNet --anno_file $anno_name --im_dir ../$raw_img_dir --save_dir ../$output_dir --epoch $pnet_epoch $rnet_epoch --prefix ../$pnet_prefix ../$rnet_prefix
-python gen_gesture.py --net ONet --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir
-python merge_gesture_and_data.py --net ONet --base_dir ../$output_dir
-python gen_tfrecord.py --net ONet --data_dir ../$output_dir
+python gen_data.py --test_mode RNet --anno_file $anno_name --im_dir ../$raw_img_dir --save_dir ../$output_dir/$net --epoch $pnet_epoch $rnet_epoch --prefix ../$net_prefix/PNet ../$rnet_prefix/RNet
+python gen_gesture.py --net $net --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir/$net
+python merge_gesture_and_data.py --net $net --base_dir ../$output_dir/$net
+python gen_tfrecord.py --net $net --data_dir ../$output_dir/$net
+cd ..
+
+echo('ONet data generation completes')
+
+# ONet training
+cd Train_Model
+python train_net --net $net --model_name $model_name --tfrecord_dir ../$output_dir/$net/$tfrecord_dir --base_lr $base_lr --end_epoch $oend_epoch
+cd ..
+
+echo('ONet training completes')
+
+echo('PRO training completes!')
+echo(model name: $model_name)
+echo(output_dir: $output_dir)
+echo(save_path: $net_prefix)
+echo(dataset: $raw_img_dir)
