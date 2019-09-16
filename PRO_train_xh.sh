7a340b45b8fbbb7fe0dca379e657d86716c71596
X Hu <xh297@cam.ac.uk>
2019-09-16 20:41:13 +0800

    ONet data generation trying

--- PRO_train_xh.sh
+++ PRO_train_xh.sh
@@ -47,7 +47,7 @@ conda activate $env_name
 
 
 # # RNet data generation
-net=RNet
+# net=RNet
 # cd prepare_ego_data
 # echo 'running gen_data.py'
 # python gen_data.py --test_mode PNet --anno_file $anno_name --im_dir ../$raw_img_dir --save_dir ../$output_dir/$net --epoch $pend_epoch --prefix ../$net_prefix/PNet
@@ -62,22 +62,22 @@ net=RNet
 # echo 'RNet data generation completes!'
 
 # RNet training
-cd Train_Model
-python train_net.py --net $net --model_name $model_name --tfrecord_dir ../$output_dir/$net/$tfrecord_dir --base_lr $r_base_lr --end_epoch $rend_epoch
-cd ..
+# cd Train_Model
+# python train_net.py --net $net --model_name $model_name --tfrecord_dir ../$output_dir/$net/$tfrecord_dir --base_lr $r_base_lr --end_epoch $rend_epoch
+# cd ..
 
 # echo 'RNet training completes'
 
-# # ONet data generation
-# net=ONet
-# cd prepare_data
-# python gen_data.py --test_mode RNet --anno_file $anno_name --im_dir ../$raw_img_dir --save_dir ../$output_dir/$net --epoch $pend_epoch $rend_epoch --prefix ../$net_prefix/PNet ../$net_prefix/RNet
-# python gen_gesture.py --net $net --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir/$net
-# python merge_gesture_and_data.py --net $net --base_dir ../$output_dir/$net
-# python gen_tfrecord.py --net $net --data_dir ../$output_dir/$net
-# cd ..
+# ONet data generation
+net=ONet
+cd prepare_data
+python gen_data.py --test_mode RNet --anno_file $anno_name --im_dir ../$raw_img_dir --save_dir ../$output_dir/$net --epoch $pend_epoch $rend_epoch --prefix ../$net_prefix/PNet ../$net_prefix/RNet
+python gen_gesture.py --net $net --im_dir ../$raw_img_dir --anno_file $anno_name --save_dir ../$output_dir/$net
+python merge_gesture_and_data.py --net $net --base_dir ../$output_dir/$net
+python gen_tfrecord.py --net $net --data_dir ../$output_dir/$net
+cd ..
 
-# echo 'ONet data generation completes'
+echo 'ONet data generation completes'
 
 # # ONet training
 # cd Train_Model
