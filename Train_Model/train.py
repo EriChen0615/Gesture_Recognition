95d6a8d95ff087a2063bacb8cae2729136a4a29a
X Hu <xh297@cam.ac.uk>
2019-09-19 03:44:53 +0800

    forgot to comment print

--- Train_Model/train.py
+++ Train_Model/train.py
@@ -310,7 +310,7 @@ def train(net_factory, prefix, end_epoch, base_dir,
             print(bbox_batch_array[0])
             print(gesture_batch_array[0])
             '''
-            print(gesture_batch_array.shape)
+            # print(gesture_batch_array.shape)
             _,_,summary = sess.run([train_op, lr_op ,summary_op], feed_dict={input_image: image_batch_array, label: label_batch_array, bbox_target: bbox_batch_array,gesture_target:gesture_batch_array})
             # _,_,summary = sess.run([train_op, lr_op ,summary_op], feed_dict={input_image: image_batch_array, label: label_batch_array, bbox_target: bbox_batch_array})
 
