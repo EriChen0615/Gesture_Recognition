3ae823be19cb7d68ccb269c1bdaadf657ac52f0f
X Hu <xh297@cam.ac.uk>
2019-09-19 16:57:38 +0800

    .

--- Train_Model/train.py
+++ Train_Model/train.py
@@ -203,7 +203,7 @@ def train(net_factory, prefix, end_epoch, base_dir,
         neg_batch_size = int(np.ceil(config.BATCH_SIZE*neg_radio))
         assert neg_batch_size != 0,"Batch Size Error "
         gesture_batch_size = int(np.ceil(config.BATCH_SIZE*gesture_radio))
-       assert gesture_batch_size >= 0,"Batch Size Error "
+        assert gesture_batch_size >= 0,"Batch Size Error "
         if (pos_batch_size+part_batch_size+neg_batch_size+gesture_batch_size)>config.BATCH_SIZE:
             gesture_batch_size = config.BATCH_SIZE - pos_batch_size - part_batch_size - neg_batch_size 
         assert pos_batch_size + part_batch_size + neg_batch_size + gesture_batch_size == config.BATCH_SIZE, "num exceeded batchsize"
